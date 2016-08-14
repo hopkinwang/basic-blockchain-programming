e3968b163717f5ebc98e6de2c1ec4ce3228a801a
hopkinwang <8836820@qq.com>
2016-08-14 07:03:57 -0700

    fix compilation error

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 OPENSSL_INCLUDE=/usr/local/opt/openssl/include/
 touch $1.c
-gcc -ansi -Wall -Wno-deprecated-declarations -I $OPENSSL_INCLUDE -pedantic -lcrypto -o $1.out $1.c
+gcc -o $1.out $1.c -ansi -Wall -Wno-deprecated-declarations -I $OPENSSL_INCLUDE -pedantic -lssl -lcrypto
 ./$1.out
